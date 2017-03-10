d13e071a24f28c4bd1164060495ffeda6e7e51b2
George Saussy <gts7@lion.zoo.cs.yale.edu>
2017-03-10 15:00:02 -0500

    Perform Test1

--- src/mslib.c
+++ src/mslib.c
@@ -1,22 +1,20 @@
 #include "mslib.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <assert.h>
 
 
 void ms_HamXVec(Hamiltonian H, Cmplx * v, Cmplx * w) {
-    Cell * iter=H.val;
+    Cell * iter=H.ptr;
 
-    while((unsigned int)(iter-H.val)<H.len) {
+    while((unsigned int)(iter-H.ptr)<H.len) {
         // Upper right part
         (w[iter->row]).re+=(iter->val).re*(v[iter->col]).re-(iter->val).im*(v[iter->col]).im;
         (w[iter->row]).im+=(iter->val).re*(v[iter->col]).im+(iter->val).re*(v[iter->col]).im;
 
         // Conjugate
-        (w[iter->col]).re+=(iter->val).re*(v[iter->row]).re+(iter->val).im*(v[iter->row]).im;
-        (w[iter->col]).im+=(iter->val).re*(v[iter->row]).im-(iter->val).im*(v[iter->row]).re;
-
+        if(iter->row!=iter->col) {
+            (w[iter->col]).re+=(iter->val).re*(v[iter->row]).re+(iter->val).im*(v[iter->row]).im;
+            (w[iter->col]).im+=(iter->val).re*(v[iter->row]).im-(iter->val).im*(v[iter->row]).re;
+        }
         iter++;
     }
 }
@@ -34,17 +32,17 @@ void ms_GenKrylovBasis(Hamiltonian H, Cmplx * v, Cmplx ** w, unsigned int m) {
 
 
 void ms_parallel_HamXVec(Hamiltonian H, Cmplx ** v, Cmplx ** w, int num) {
-    Cell * iter=H.val;
+    Cell * iter=H.ptr;
     int count=0;
 
-    while((unsigned int)(iter-H.val)<H.len) {
+    while((unsigned int)(iter-H.ptr)<H.len) {
         count=0;
         while(count<num) {
-            (w[count][iter->row]).re+=(iter->val).re*(v[iter->col]).re-(iter->val).im*(v[count][iter->col]).im;
-            (w[count][iter->row]).im+=(iter->val).re*(v[iter->col]).im+(iter->val).re*(v[count][iter->col]).im;
+            (w[count][iter->row]).re+=(iter->val).re*(v[count][iter->col]).re-(iter->val).im*(v[count][iter->col]).im;
+            (w[count][iter->row]).im+=(iter->val).re*(v[count][iter->col]).im+(iter->val).re*(v[count][iter->col]).im;
 
-            (w[count][iter->col]).re+=(iter->val).re*(v[iter->row]).re+(iter->val).im*(v[count][iter->row]).im;
-            (w[count][iter->col]).im+=(iter->val).re*(v[iter->row]).im-(iter->val).im*(v[count][iter->row]).re;
+            (w[count][iter->col]).re+=(iter->val).re*(v[count][iter->row]).re+(iter->val).im*(v[count][iter->row]).im;
+            (w[count][iter->col]).im+=(iter->val).re*(v[count][iter->row]).im-(iter->val).im*(v[count][iter->row]).re;
 
             count++;
         }
@@ -52,6 +50,7 @@ void ms_parallel_HamXVec(Hamiltonian H, Cmplx ** v, Cmplx ** w, int num) {
     }
 }
 
+/*
 void ms_parallel_GenKrylovBasis(Hamiltonian H, Cmplx ** v, Cmplx *** w, unsigned int m, int num) {
     unsigned int count=0;
 
@@ -61,3 +60,4 @@ void ms_parallel_GenKrylovBasis(Hamiltonian H, Cmplx ** v, Cmplx *** w, unsigned
         count++;
     }
 }
+*/
--- src/mslib.h
+++ src/mslib.h
@@ -16,7 +16,7 @@ typedef struct Hamiltonian {
     unsigned int N;
     unsigned int len;
     Cell * ptr;
-} Matrix;
+} Hamiltonian;
 
 /**
     Multiply a vector by a Hamiltonian and store the result at a given location
@@ -51,7 +51,7 @@ void ms_GenKrylovBasis(Hamiltonian H, Cmplx * v, Cmplx ** w, unsigned int m);
 void ms_parallel_HamXVec(Hamiltonian H, Cmplx ** v, Cmplx ** w, int num);
 
 /**
-    Generate the Krylov basis for a Hamiltonian and set of vector and store the 
+    Generate the Krylov basis for a Hamiltonian and set of vector and store the
     result in a a given location
     @param H the Hamiltonian
     @param v the vectors
@@ -61,6 +61,6 @@ void ms_parallel_HamXVec(Hamiltonian H, Cmplx ** v, Cmplx ** w, int num);
     calculated
     @return void
 */
-void ms_parallel_GenKrylovBasis(Hamiltonian H, Cmplx ** v, Cmplx *** w, unsigned int m, int num) {
+//void ms_parallel_GenKrylovBasis(Hamiltonian H, Cmplx ** v, Cmplx *** w, unsigned int m, int num) {
 
 #endif
