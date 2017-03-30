1727afb7b23bc5758dc34e7ba6485e1a10a996c9
George Saussy <gts7@macaw.zoo.cs.yale.edu>
2017-03-30 04:06:13 -0400

    checkpoint

--- src/speedtests.imp1.c
+++ src/speedtests.imp1.c
@@ -3,6 +3,7 @@
 #include "math.h"
 #include "time.h"
 #include "assert.h"
+#include "mkl.h"
 
 // STRUCTS TO AID IN COMPUTATION
 
@@ -32,10 +33,10 @@ typedef struct Hamiltonian {
     @param w where to store the result, should be maqlloc'd and zeroed
     @return void
 */
-void ms_HamXVec(Hamiltonian H, Cmplx * v, Cmplx * w) {
-    Cell * iter=H.ptr;
+void ms_HamXVec(Hamiltonian * H, Cmplx * v, Cmplx * w) {
+    Cell * iter=H->ptr;
 
-    while((unsigned int)(iter-H.ptr)<H.len) {
+    while((unsigned int)(iter-H->ptr)<H->len) {
         (w[iter->row]).re+=(iter->val).re*(v[iter->col]).re-(iter->val).im*(v[iter->col]).im;
         (w[iter->row]).im+=(iter->val).re*(v[iter->col]).im+(iter->val).re*(v[iter->col]).im;
 
@@ -52,7 +53,7 @@ void ms_HamXVec(Hamiltonian H, Cmplx * v, Cmplx * w) {
     @param m the number of Kylov basis to calculate, must be > 1, should be > 10
     @return void
 */
-void ms_GenKrylovBasis(Hamiltonian H, Cmplx * v, Cmplx ** w, unsigned int m) {
+void ms_GenKrylovBasis(Hamiltonian * H, Cmplx * v, Cmplx ** w, unsigned int m) {
     unsigned int count=1;
 
     ms_HamXVec(H, v, w[0]);
@@ -149,6 +150,134 @@ Hamiltonian  ms_generateRandomHamiltonian(int sp, int dim) {
         ((ret.ptr)[j]).val.im=1;
     }
     return ret;
+}
+
+/**
+
+*/
+double vec_norm(Cmplx * p, int dim) {
+    double ret=0;
+    for(int k=0;k<dim;k++) {
+        ret+=p[k].re*p[k].re+p[k].im*p[k].im;
+    }
+    ret=sqrt(ret);
+    return ret;
+}
+
+/**
+    Implement Krylov exponentiation
+    @param t time
+    @param A a pointer to a Hamiltonian
+    @param v the vector, norm must be == 1
+    @paran ans where to store the answer ( Dim(ans) == Dim(v) == A->N )
+*/
+void ms_expv(double t, Hamiltonian * A, Cmplx * v, int m, Cmplx * ans) { // TODO should pass this V, H, F, p
+    /*
+        double pi=M_PI;
+
+        int in=A->N;
+        double tol=0.0000001;
+        int m=30; // FIXME should be mutable
+        int anorm=anorm(A); // TODO
+        int mxrej=210;
+        double btol=0.0000001;
+        double gamma=0.9;
+        double delta=1.2;
+        int mb=m;
+        double t_out=abs(t);
+        double s_error=0;
+        double rndoff=anorm*eps; // XXX eps?
+
+        int k1=2;
+        double xm=1/m;
+        double normv=1.0; // norm(v)
+        double beta=normv;
+        double fact=pow(((m+1)/exp(1)),(m+1))*sqrt(2*pi*(m+1));
+        double t_new=(1/anorm)*pow(((fact*tol)/(4*beta*anorm)),xm);
+        double s=pow(10,floor(log10(t_new))-1);
+        t_new=ceil(t_new/s)*s;
+        int sgn=abs(t)/t;
+        int nstep=0;
+
+        while(t_now<t_out) {
+            nstep++;
+
+
+            mx=mb;
+            if(k1-1>0) mx+=k1-1;
+            // beta=norm(w);
+            if(beta>hump) hump=beta;
+
+            t_now=t_now+t_step;
+            t_new=gama*t_step*pow(t_step*tol/err_loc,xm);
+            s=pow(10,floor(log10(t_new))-1);
+            t_new=ceil(t_new/s)*s;
+
+            if(rndoff>err_loc) err_loc=rndoff;
+            s_error=s_error+err_loc;
+        }
+        err=s_error;
+        // hump=hump/normv;
+    */
+    // constants
+    double btol=0.0000001;
+
+    // reserving some space
+    int dim=A->N;
+    Cmplx zero; zero.re=0.0; zero.im=0.0;
+
+    // the expensive mallocs
+    Cmplx ** V;
+    V=(Cmplx**)malloc(dim*sizeof(Cmplx*));
+    for(int k=0;k<dim;k++) {
+        V[k]=(Cmplx*)malloc((m+1)*sizeof(Cmplx));
+    }
+    Cmplx ** H;
+    H=(Cmplx**)malloc((m+2)*sizeof(Cmplx*));
+    for(int k=0;k<dim;k++) {
+        H[k]=(Cmplx*)malloc((m+2)*sizeof(Cmplx*));
+    }
+    Cmplx ** F;
+    F=(Cmplx*)malloc(dim*sizeof(Cmplx*));
+    for(int k=0;k<dim;k++) {
+        F[k]=(Cmplx*)malloc(sizeof(Cmplx));
+    }
+    Cmplx * p=(Cmplx*)malloc(dim*sizeof(Cmplx));
+
+    ms_HamXVec(A, v, V[0]);
+    for(j=1;j<m;j++) {
+        ms_HamXVec(A, V[j], p);
+        for(i=0;i<j;i++) {
+            // H[i][j]=V[:][i]''*p;
+            H[i][j]=zero;
+            for(int k1=0;k1<dim;k1++) {
+                H[i][j].re+=V[i][k1].re*p[k1].re-V[i][k1].im*p[k1].im;
+                H[i][j].im+=V[i][k1].re*p[k1].im+V[i][k1].im*p[k1].re;
+            }
+
+            // p=p-H[i][j]*V[:][i];
+            for(int k1=0;k1<dim;k1++) {
+                p[k1].re-=H[i][j].re*V[k1][i].re-H[i][j].im*V[i][k1].im;
+                p[k1].im-=H[i][j].re*V[k1][i].im+H[i][j].im*V[i][k1].re;
+            }
+        }
+
+        s=vec_norm(p, dim);
+        if(s<btol) {
+            K1=0;
+            mb=j;
+            t_step=t_out-t_new;
+        }
+
+        H[j+1][j]=s;
+        // V[:][j+1]=1/s*p;
+        for(int k1=0;k1<dim;k1++) {
+            V[j+1][k1]=1/s*p[k1];
+        }
+
+        ms_matexp(tau, H, F);
+    }
+
 }
 
 // IMPLEMENTATION 1 TESTS
