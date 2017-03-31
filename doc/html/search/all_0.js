10720de9d0c4294685bddec43dce343b3aa2c867
George Saussy <george.saussy@yale.edu>
2017-03-31 07:49:15 +0000

    Add doxygen documentation

---
+++ doc/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['cell',['Cell',['../structCell.html',1,'']]],
+  ['cmplx',['Cmplx',['../structCmplx.html',1,'']]]
+];
---
+++ doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hamiltonian',['Hamiltonian',['../structHamiltonian.html',1,'']]]
+];
