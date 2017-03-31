9662dd04ba8274af58b8fc4810829f8dca3852b9
George Saussy <gts7@macaw.zoo.cs.yale.edu>
2017-03-31 03:17:09 -0400

    Finish implementation 1

--- src/speedtests.imp1.c
+++ src/speedtests.imp1.c
@@ -3,7 +3,8 @@
 #include "math.h"
 #include "time.h"
 #include "assert.h"
-#include "mkl.h"
+/** @file */
+
 
 // STRUCTS TO AID IN COMPUTATION
 
@@ -153,7 +154,10 @@ Hamiltonian  ms_generateRandomHamiltonian(int sp, int dim) {
 }
 
 /**
-
+    Calculate the norm of a vector
+    @param p the vector
+    @param dim the dimention of the vector
+    @return the norm
 */
 double vec_norm(Cmplx * p, int dim) {
     double ret=0;
@@ -163,68 +167,92 @@ double vec_norm(Cmplx * p, int dim) {
     ret=sqrt(ret);
     return ret;
 }
+/**
+    Compute a matrix exponential
+    @param H the matrix to exponentiate as a (Cmplx**)
+    @param F where to store the result
+    @param S a workspace to store a matrix
+    @param p a workspace to store a vector
+    @param dim the dimention of the matrix
+    @param iter the number of iterations of the matrix exponential to perform
+    (must be >0)
+    @return void
+*/
+void ms_expm(Cmplx ** H, Cmplx ** F, Cmplx ** S, Cmplx * p, int dim, int iter) {
+    Cmplx zero; zero.re=0.0; zero.im=0.0;
+    int k, k1, k2, i;
+    double fac;
+
+    for(k=0;k<iter;k++) {
+        for(k1=0;k1<dim;k1++) {
+            F[k][k1]=zero;
+            S[k][k1]=zero;
+            if(k==k1) {
+                F[k][k1].re=1.0;
+                S[k][k1].re=1.0;
+            }
+        }
+    }
+    fac=1;
+    for(int i=1;i<iter;i++) {
+        fac*=i;
+        if(i==1) {
+            for(k=0;k<dim;k++) {
+                for(k1=0;k1<dim;k1++) {
+                    S[k][k1]=H[k][k1];
+                }
+            }
+        }
+        else {
+            for(k=0;k<dim;k++) {
+                for(k1=0;k1<dim;k1++) {
+                    p[k1]=zero;
+                }
+                for(k1=0;k1<dim;k1++) {
+                    for(k2=0;k2<dim;k2++) {
+                        p[k1].re+=H[k1][k2].re*S[k][k2].re-H[k1][k2].im*S[k][k2].im;
+                        p[k1].im+=H[k1][k2].re*S[k][k2].im+H[k1][k2].im*S[k][k2].re;
+                    }
+                }
+                for(k1=0;k1<dim;k1++) {
+                    S[k][k1]=p[k1];
+                }
+            }
+        }
+        for(k=0;k<dim;k++) {
+            for(k1=0;k1<dim;k1++) {
+                F[k][k1].re+=S[k][k1].re/fac;
+                F[k][k1].im+=S[k][k1].im/fac;
+            }
+        }
+    }
+}
 
 /**
     Implement Krylov exponentiation
     @param t time
     @param A a pointer to a Hamiltonian
+    @param normA the infinity norm of A (should be precomputed to save time)
     @param v the vector, norm must be == 1
-    @paran ans where to store the answer ( Dim(ans) == Dim(v) == A->N )
+    @paran w where to store the answer ( Dim(ans) == Dim(v) == A->N )
 */
-void ms_expv(double t, Hamiltonian * A, Cmplx * v, int m, Cmplx * ans) { // TODO should pass this V, H, F, p
-    /*
-        double pi=M_PI;
-
-        int in=A->N;
-        double tol=0.0000001;
-        int m=30; // FIXME should be mutable
-        int anorm=anorm(A); // TODO
-        int mxrej=210;
-        double btol=0.0000001;
-        double gamma=0.9;
-        double delta=1.2;
-        int mb=m;
-        double t_out=abs(t);
-        double s_error=0;
-        double rndoff=anorm*eps; // XXX eps?
-
-        int k1=2;
-        double xm=1/m;
-        double normv=1.0; // norm(v)
-        double beta=normv;
-        double fact=pow(((m+1)/exp(1)),(m+1))*sqrt(2*pi*(m+1));
-        double t_new=(1/anorm)*pow(((fact*tol)/(4*beta*anorm)),xm);
-        double s=pow(10,floor(log10(t_new))-1);
-        t_new=ceil(t_new/s)*s;
-        int sgn=abs(t)/t;
-        int nstep=0;
-
-        while(t_now<t_out) {
-            nstep++;
-
-
-            mx=mb;
-            if(k1-1>0) mx+=k1-1;
-            // beta=norm(w);
-            if(beta>hump) hump=beta;
-
-            t_now=t_now+t_step;
-            t_new=gama*t_step*pow(t_step*tol/err_loc,xm);
-            s=pow(10,floor(log10(t_new))-1);
-            t_new=ceil(t_new/s)*s;
-
-            if(rndoff>err_loc) err_loc=rndoff;
-            s_error=s_error+err_loc;
-        }
-        err=s_error;
-        // hump=hump/normv;
-    */
+void ms_expv(double t, Hamiltonian * A, double normA, Cmplx * v, int m, Cmplx * w) { // TODO should pass this V, H, F, p
     // constants
     double btol=0.0000001;
+    double tol=btol;
+    const double eps=pow(10.0,-15); // fudged machine tolerance
+    const int exp_iter=5;
 
     // reserving some space
-    int dim=A->N;
+    const int dim=A->N;
     Cmplx zero; zero.re=0.0; zero.im=0.0;
+    Cmplx one; one.re=0.0; one.im=0.0;
+    int K1=2, mxrej=10, mb=m, sgn=abs(t)/t, nstep=0;;
+    int ireject, mx;
+    double s, phi1, phi2, t_step, t_out, t_new, avnorm, normv=1.0, beta=1.0;
+    double delta=1.2, xm=1.0/m, gamma=0.9, hump=1.0, t_now=0.0, err;
+    double anorm=normA, rndoff=anorm*eps, s_error=0.0;
+    double err_loc; // local error
 
     // the expensive mallocs
     Cmplx ** V;
@@ -238,46 +266,136 @@ void ms_expv(double t, Hamiltonian * A, Cmplx * v, int m, Cmplx * ans) { // TODO
         H[k]=(Cmplx*)malloc((m+2)*sizeof(Cmplx*));
     }
     Cmplx ** F;
-    F=(Cmplx*)malloc(dim*sizeof(Cmplx*));
+    F=(Cmplx**)malloc(dim*sizeof(Cmplx*));
     for(int k=0;k<dim;k++) {
         F[k]=(Cmplx*)malloc(sizeof(Cmplx));
     }
-    Cmplx * p=(Cmplx*)malloc(dim*sizeof(Cmplx));
-
-    ms_HamXVec(A, v, V[0]);
-    for(j=1;j<m;j++) {
-        ms_HamXVec(A, V[j], p);
-        for(i=0;i<j;i++) {
-            // H[i][j]=V[:][i]''*p;
-            H[i][j]=zero;
-            for(int k1=0;k1<dim;k1++) {
-                H[i][j].re+=V[i][k1].re*p[k1].re-V[i][k1].im*p[k1].im;
-                H[i][j].im+=V[i][k1].re*p[k1].im+V[i][k1].im*p[k1].re;
-            }
-
-            // p=p-H[i][j]*V[:][i];
-            for(int k1=0;k1<dim;k1++) {
-                p[k1].re-=H[i][j].re*V[k1][i].re-H[i][j].im*V[i][k1].im;
-                p[k1].im-=H[i][j].re*V[k1][i].im+H[i][j].im*V[i][k1].re;
-            }
-        }
-
-        s=vec_norm(p, dim);
-        if(s<btol) {
-            K1=0;
-            mb=j;
-            t_step=t_out-t_new;
-        }
-
-        H[j+1][j]=s;
-        // V[:][j+1]=1/s*p;
-        for(int k1=0;k1<dim;k1++) {
-            V[j+1][k1]=1/s*p[k1];
-        }
-
-        ms_matexp(tau, H, F);
+    Cmplx ** S;
+    S=(Cmplx**)malloc(dim*sizeof(Cmplx*));
+    for(int k=0;k<dim;k++) {
+        S[k]=(Cmplx*)malloc(sizeof(Cmplx));
     }
+    Cmplx * p=(Cmplx*)malloc(dim*sizeof(Cmplx)); // workspace
+    for(int k=0;k<dim;k++) {
+        w[k]=v[k];
+    }
+    Cmplx * workspace=(Cmplx*)malloc(dim*sizeof(Cmplx));
 
+    hump=normv;
+    while(t_now<t_out) {
+        nstep=nstep+1;
+        t_step=t_out-t_now;
+        if(t_new<t_step) t_step=t_new;
+        // V=zeros(n,m+1);
+        for(int k=0;k<m+1;k++) {
+            for(int k1=0;k1<dim;k1++) {
+                V[k][k1]=zero;
+            }
+        }
+        // H=zeros(m+2,m+2);
+        for(int k=0;k<m+2;k++) {
+            for(int k1=0;k1<m;k1++) {
+                H[k][k1]=zero;
+            }
+        }
+        for(int j=1;j<m;j++) {
+            ms_HamXVec(A, V[j], p);
+            for(int i=0;i<j;i++) {
+                // H[i][j]=V[:][i]''*p;
+                H[i][j]=zero;
+                for(int k=0;k<dim;k++) {
+                    H[i][j].re+=V[i][k].re*p[k].re-V[i][k].im*p[k].im;
+                    H[i][j].im+=V[i][k].re*p[k].im+V[i][k].im*p[k].re;
+                }
+
+                // p=p-H[i][j]*V[:][i];
+                for(int k=0;k<dim;k++) {
+                    p[k].re-=H[i][j].re*V[k][i].re-H[i][j].im*V[i][k].im;
+                    p[k].im-=H[i][j].re*V[k][i].im+H[i][j].im*V[i][k].re;
+                }
+            }
+            s=vec_norm(p, dim);
+            if(s<btol) {
+                K1=0;
+                mb=j;
+                t_step=t_out-t_new;
+                break;
+            }
+            H[j+1][j].re=s;
+            H[j+1][j].im=0.0;
+            // V[:][j+1]=1/s*p;
+            for(int k=0;k<dim;k++) {
+                V[j+1][k].re=1/s*p[k].re;
+                V[j+1][k].im=1/s*p[k].im;
+            }
+        }
+        if(K1!=0) {
+            H[m+1][m+1]=one;
+            // avnorm=vec_norm(A*V(:,m+1));
+    	    ms_HamXVec(A,V[m],workspace);
+    	    avnorm=vec_norm(workspace,dim);
+        }
+        ireject=0;
+        while(ireject<=mxrej) {
+            mx=mb+K1;
+            // F=expm(sgn*t_step*H(1:,mx,1:mx));
+            ms_expm(H,F,S,workspace,mx,exp_iter);
+            if(K1==0) {
+                err_loc=btol;
+                break;
+            }
+            else {
+                phi1 = abs(beta)*sqrt(F[0][m].re*F[0][m].re+F[0][m].im*F[0][m].im);
+                phi2 = abs(beta*avnorm)*sqrt(F[0][m+1].re*F[0][m+1].re+F[0][m+1].im*F[0][m+1].im);
+                if(phi1>10*phi2) {
+                    err_loc=phi2;
+                    xm=1.0/m;
+                }
+                else if(phi1>phi2) {
+                    err_loc = (phi1*phi2)/(phi1-phi2);
+                    xm = 1.0/m;
+                }
+                else {
+                    err_loc = phi1;
+                    xm = 1.0/(m-1);
+                }
+            }
+            if(err_loc<=delta*t_step*tol) {
+                break;
+            }
+            else {
+                t_step=pow(gamma*t_step*(t_step*tol/err_loc),xm);
+                s=pow(10,(floor(log10(t_step))-1));
+                t_step=ceil(t_step/s)*s;
+                if(ireject==mxrej) {
+                    fprintf(stderr,"The requested tolerance is too high.\n");
+                }
+                ireject+=1;
+            }
+        }
+        mx = mb;
+        if(K1>1) mx+=K1;
+        // w = V(:,1:mx)*(beta*F(1:mx,1));
+        for(int k=0;k<dim;k++) {
+            w[k]=zero;
+            for(int k1=0;k1,mx;k1++) {
+                w[k].re+=beta*(F[1][k1].re*V[k][k1].re-F[1][k1].im*V[k][k1].im);
+                w[k].im+=beta*(F[1][k1].re*V[k][k1].im+F[1][k1].im*V[k][k1].re);
+            }
+        }
+        beta=vec_norm(w,dim);
+        if(beta>hump) hump=beta;
+        t_now=t_now+t_step;
+
+        t_new=pow(gamma*t_step*(t_step*tol/err_loc),xm);
+        s=pow(10,(floor(log10(t_new))-1));
+        t_new=ceil(t_new/s)*s;
+
+        if(rndoff>err_loc) err_loc=rndoff;
+        s_error=s_error+err_loc;
+    }
+    err=s_error;
+    hump=hump/normv;
 }
 
 // IMPLEMENTATION 1 TESTS
@@ -338,7 +456,7 @@ void ms_test3_GenKrylovBasis(int lowdim, int hidim, int skip, int iter) {
                 }
             }
             begin = clock();
-                ms_GenKrylovBasis(H, v, w, m);
+                ms_GenKrylovBasis(&H, v, w, m);
             end = clock();
             avg += (double)(end - begin);
         }
@@ -393,7 +511,7 @@ void ms_test4_GenKrylovBasis(int lowdim, int hidim, int skip, int iter, int spar
             }
             begin = clock();
                 //printf("Hello1\n");
-                ms_GenKrylovBasis(H, v, w, m);
+                ms_GenKrylovBasis(&H, v, w, m);
                 //printf("Hello2\n");
             end = clock();
             avg += (double)(end - begin);
